8caba98b4f63a1d58302712270ce30afc132e89f
Sivakornsir <141816219+Sivakornsir@users.noreply.github.com>
2023-12-19 15:06:17 +0700

    lab10_2

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,11 +6,21 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string line;
+    if (source.is_open() && dest.is_open()) {
+        dest << "-------------------- BOOM ---------------------" << std::endl;
+
+        while (getline(source, line)) {
+            dest << line << std::endl;
+        }
+
+        dest << "-------------------- HA!! ---------------------" << std::endl;
+    } else {
+        std::cout << "Unable to open file";
+    }
 
     source.close();
     dest.close();
